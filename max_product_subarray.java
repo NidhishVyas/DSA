1b50f699c9c7d2214c7023ceddb7291d520d83fc
NidhishVyas <vyasnidhish2001@gmail.com>
2026-02-01 22:22:25 -0500

    Add implementations for max product subarray and product of array except self algorithms

---
+++ max_product_subarray.java
@@ -0,0 +1,31 @@
+//brute force
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProduct = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = i; j < nums.length; j++) {
+                product *= nums[j];
+                maxProduct = Math.max(maxProduct, product);
+            }
+        }
+        return maxProduct;
+    }
+}
+
+//optimal
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix = 1, suffix = 1;
+        int maxProduct = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
+            prefix *= nums[i];
+            suffix *= nums[n - 1 - i];
+            maxProduct = Math.max(maxProduct, Math.max(prefix, suffix));
+        }
+        return maxProduct;
+    }
+}
